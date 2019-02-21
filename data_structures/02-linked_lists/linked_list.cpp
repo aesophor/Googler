49cce64bc932100a6853fda69d13ee4aabf829c5
Marco Wang <m.aesophor@gmail.com>
2019-02-21 19:14:11 +0800

    Initial commit

---
+++ data_structures/02-linked_lists/linked_list.cpp
@@ -0,0 +1,177 @@
+#include "linked_list.hpp"
+
+template <typename T>
+LinkedList<T>::LinkedList() : head_(), tail_(), size_(0) {}
+
+template <typename T>
+LinkedList<T>::~LinkedList() {}
+
+
+template <typename T>
+int LinkedList<T>::size() const {
+    return size_;
+}
+
+template <typename T>
+int LinkedList<T>::empty() const {
+    return size_ == 0;
+}
+
+template <typename T>
+T LinkedList<T>::at(int index) const {
+    int i = 0;
+    for (Node* ptr = head_; ptr; ptr = ptr->next, i++) {
+        if (i == index) {
+            return ptr->data;
+        }
+    }
+}
+
+template <typename T>
+T LinkedList<T>::front() const {
+    return head_;
+}
+
+template <typename T>
+T LinkedList<T>::back() const {
+    return tail_;
+}
+
+
+template <typename T>
+void LinkedList<T>::push_front(T value) {
+    Node* new_node = new Node(value);
+
+    if (!head_) {
+        head_ = new_node;
+        tail_ = new_node;
+    } else {
+        new_node->next = head_;
+        head_->prev = new_node;
+    }
+
+    head_ = new_node;
+    size_++;
+}
+
+template <typename T>
+void LinkedList<T>::push_back(T value) {
+    Node* new_node = new Node(value);
+
+    if (!tail_) {
+        head_ = new_node;
+        tail_ = new_node;
+    } else {
+        new_node->prev = tail_;
+        tail_->next = new_node;
+    }
+
+    tail_ = new_node;
+    size_++;
+}
+
+template <typename T>
+T LinkedList<T>::pop_front() {
+    if (head_) {
+        Node* old_head = head_;
+        T old_head_data = old_head->data;
+
+        head_ = old_head->next;
+        if (head_) {
+            head_->prev = nullptr;
+        } else {
+            tail_ = nullptr;
+        }
+        delete old_head;
+        size_--;
+        return old_head_data;
+    }
+}
+
+template <typename T>
+T LinkedList<T>::pop_back() {
+    if (tail_) {
+        Node* old_tail = tail_;
+        T old_tail_data = old_tail->data;
+
+        tail_ = old_tail->prev;
+        if (tail_) {
+            tail_->next = nullptr;
+        } else {
+            head_ = nullptr;
+        }
+        delete old_tail;
+        size_--;
+        return old_tail_data;
+    }
+}
+
+template <typename T>
+void LinkedList<T>::insert(int index, T value) {
+    int i = 0;
+    for (Node* ptr = head_; ptr; ptr = ptr->next, i++) {
+        if (i == index - 1) {
+            Node* new_node = new Node(value);
+            ptr->next->prev = new_node;
+            new_node->next = ptr->next;
+            ptr->next = new_node;
+            new_node->prev = ptr;
+            size_++;
+        }
+    }
+}
+
+template <typename T>
+void LinkedList<T>::erase(int index) {
+    int i = 0;
+    for (Node* ptr = head_; ptr; ptr = ptr->next, i++) {
+        if (i == index) {
+            Node* target_node = ptr;
+            ptr->prev->next = ptr->next;
+            ptr->next->prev = ptr->prev;
+            delete target_node;
+            size_--;
+            return;
+        }
+    }
+}
+
+template <typename T>
+void LinkedList<T>::remove_value(T value) {
+    for (Node* ptr = head_; ptr; ptr = ptr->next) {
+        if (ptr->data == value) {
+            Node* target_node = ptr;
+            ptr->prev->next = ptr->next;
+            ptr->next->prev = ptr->prev;
+            delete target_node;
+            size_--;
+            return;
+        }
+    }
+}
+
+template <typename T>
+void LinkedList<T>::reverse() {
+    // For each node, swap prev and next pointers.
+    Node* ptr = head_;
+    while (ptr) {
+        Node* old_prev = ptr->prev;
+        ptr->prev = ptr->next;
+        ptr->next = old_prev;
+        ptr = ptr->prev;
+    }
+
+    // Swap head and tail pointers.
+    Node* old_head = head_;
+    head_ = tail_;
+    tail_ = old_head;
+}
+
+template <typename T>
+std::string LinkedList<T>::ToString() const {
+    std::string s;
+    for (Node* ptr = head_; ptr; ptr = ptr->next) {
+        s += std::to_string(ptr->data) + " ";
+    }
+    return s;
+}
---
+++ data_structures/02-linked_lists/linked_list.hpp
@@ -0,0 +1,44 @@
+#ifndef LINKED_LIST_HPP_
+#define LINKED_LIST_HPP_
+
+#include <string>
+
+template <typename T>
+class LinkedList {
+public:
+    LinkedList();
+    ~LinkedList();
+
+    int size() const;
+    int empty() const;
+    T at(int index) const;
+    T front() const;
+    T back() const;
+
+    void push_front(T value);
+    void push_back(T value);
+    T pop_front();
+    T pop_back();
+
+    void insert(int index, T value);
+    void erase(int index);
+    void remove_value(T value);
+    void reverse();
+
+    std::string ToString() const;
+
+private:
+    struct Node {
+        Node(T value) : prev(), next(), data(value) {}
+        ~Node() {}
+        Node* prev;
+        Node* next;
+        T data;
+    };
+
+    LinkedList::Node* head_;
+    LinkedList::Node* tail_;
+    int size_;
+};
+
+#endif
