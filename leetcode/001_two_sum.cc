e669342ea30d19262065510c024f7102d76819d3
Marco Wang <m.aesophor@gmail.com>
2019-03-07 17:55:40 +0800

    O(1) solution to two sum

--- leetcode/001_two_sum.cc
+++ leetcode/001_two_sum.cc
@@ -1,25 +1,23 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
+// Runtime: 12 ms, faster than 95.04% of C++ online submissions for Two Sum.
+// Memory Usage: 10.4 MB, less than 23.34% of C++ online submissions for Two Sum.
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < (int) nums.size(); i++) {
-            for (int j = i + 1; j < (int) nums.size(); j++) {
-                if (nums.at(i) + nums.at(j) == target) {
-                    return vector<int> { i, j };
-                }
+        unordered_map<int, int> complement_indices;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (complement_indices.find(complement) != complement_indices.end()) {
+                // If the complement of nums[i] can be found, return it and i.
+                return { complement_indices.at(complement), i };
+            } else {
+                // Record num's complement in the hash map.
+                complement_indices[nums[i]] = i;    
             }
         }
-        return vector<int> { -1, -1 };
+        
+        return { 0, 0 }; // The question didn't specify what to return if not found
     }
 };
-
-int main() {
-    vector<int> input = { 3, 2, 4 };
-    for (auto num : Solution().twoSum(input, 6)) {
-        cout << num << endl;
-    }
-}
