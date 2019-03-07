744e56d54b0e93a251b8f97d3f68f11f073f9082
Marco Wang <m.aesophor@gmail.com>
2019-03-07 10:46:27 +0800

    optimize memory efficiency

--- leetcode/705_to_lower.cpp
+++ leetcode/705_to_lower.cpp
@@ -1,9 +1,9 @@
 // Runtime: 4 ms, faster than 100.00% of C++ online submissions for To Lower Case.
-// Memory Usage: 8.3 MB, less than 29.58% of C++ online submissions for To Lower Case.
+// Memory Usage: 8.3 MB, less than 47.92% of C++ online submissions for To Lower Case.
 
 class Solution {
 public:
-    string toLowerCase(string str) {
+    string toLowerCase(string& str) {
         for (auto& c : str) {
             if (c >= 'A' && c <= 'Z') {
                 c += 'a' - 'A';
