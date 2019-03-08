d849035030e15ed71941878fd30e18d1a9eb7b43
Marco Wang <m.aesophor@gmail.com>
2019-03-08 17:11:40 +0800

    @_@

--- leetcode/024_swap_nodes_in_pairs.cc
+++ leetcode/024_swap_nodes_in_pairs.cc
@@ -15,35 +15,35 @@ public:
         if (!head) {
             return nullptr;
         }
-        
+
         pair<ListNode*, ListNode*> current_pair = { head, head->next }; // in original order
         ListNode* ret = (current_pair.second) ? current_pair.second : current_pair.first;
-        
+
         while (current_pair.first && current_pair.second) {
             // Cache the next pair if it exists.
             pair<ListNode*, ListNode*> next_pair = { nullptr, nullptr };
-            
+
             if (current_pair.second) {
                 next_pair.first = current_pair.second->next;
                 if (current_pair.second->next) {
                     next_pair.second = current_pair.second->next->next;
                 }
             }
-            
-            if (next_pair.first && !next_pair.second) {
-                current_pair.second->next = current_pair.first;
-                current_pair.first->next = next_pair.first;
-                break;
-            }
-            
+
             // Re-link current pair
             current_pair.second->next = current_pair.first;
-            current_pair.first->next = next_pair.second;
-            
-            current_pair.second = next_pair.second;
-            current_pair.first = next_pair.first;
+
+            // If there's only one node in next pair
+            if (next_pair.first && !next_pair.second) {
+                current_pair.first->next = next_pair.first;
+                break;
+            } else {
+                current_pair.first->next = next_pair.second;
+                current_pair.second = next_pair.second;
+                current_pair.first = next_pair.first;
+            }
         }
-        
+
         return ret;
     }
 };
