e02c7e17595e38d381eacc6e4197847ce4b3d723
Marco Wang <m.aesophor@gmail.com>
2019-03-11 10:53:39 +0800

    pass object by value causes destructor to be called at the end

---
+++ Snippets/cpp/destructor_double_call.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class A {
+public:
+    A(char id, char* text) : id_(id) {
+        cout << "Constructor called" << endl;
+        text_ = new char[strlen(text) + 1];
+        strcpy(text_, text);
+    }
+
+    virtual ~A() {
+        cout << "Destructor called" << endl;
+        delete[] text_;
+    }
+
+    friend ostream& operator<< (ostream& os, const A& a);
+
+private:
+    char id_;
+    char* text_;
+};
+
+ostream& operator<< (ostream& os, const A& a) {
+    return os << "ID: " << a.id_ << " " << a.text_; 
+}
+
+
+/*
+void DisplayTitle(A* a) {
+    a->Show();
+}
+*/
+
+void DisplayTitle(A& a) {
+    cout << a << endl;
+}
+
+int main() {
+    A* a = new A('A', "Some interesting title");
+    DisplayTitle(*a);
+    DisplayTitle(*a);
+    delete a;
+}
