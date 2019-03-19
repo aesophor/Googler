f0ae2e1ec4cf2a2fdcd47b17329fc32e98f514c5
Marco Wang <m.aesophor@gmail.com>
2019-03-19 22:37:51 +0800

    reduced to summing a tree using postorder

--- LeetCodeOJ/559_binary_tree_tilt.cc
+++ LeetCodeOJ/559_binary_tree_tilt.cc
@@ -13,23 +13,24 @@
 class Solution {
 public:
     int findTilt(TreeNode* root) {
-        postOrder(root);
+        sumSubtree(root);
         return result_;
     }
-    
+
 private:
-    int postOrder(TreeNode* root) {
+    int sumSubtree(TreeNode* root) {
         if (!root) {
             return 0;
         }
-        
+
+        // This problem can be reduced to "sum a tree using postorder and recursion".
         // Look at this, we want to get the sum of entire left subtree, right?
         // So we have to return root->left's left subtree + root->left + root_left's right subtree
-        int left_subtree_sum = postOrder(root->left);
-        int right_subtree_sum = postOrder(root->right);
+        int left_subtree_sum = sumSubtree(root->left);
+        int right_subtree_sum = sumSubtree(root->right);
         result_ += std::abs(left_subtree_sum - right_subtree_sum);
         return left_subtree_sum + root->val + right_subtree_sum;
     }
-    
+
     int result_ = 0;
 };
