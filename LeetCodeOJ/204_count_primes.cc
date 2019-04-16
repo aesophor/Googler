102b9124d8a30581c2edf32cef5e506bc6c8812b
Marco Wang <m.aesophor@gmail.com>
2019-04-16 15:59:24 +0800

    唉

--- LeetCodeOJ/204_count_primes.cc
+++ LeetCodeOJ/204_count_primes.cc
@@ -1,40 +1,33 @@
-// Runtime: 36 ms, faster than 83.61% of C++ online submissions for Count Primes.
-// Memory Usage: 9.7 MB, less than 33.77% of C++ online submissions for Count Primes.
+// Runtime: 24 ms, faster than 94.84% of C++ online submissions for Count Primes.
+// Memory Usage: 9.8 MB, less than 31.79% of C++ online submissions for Count Primes.
 
 class Solution {
 public:
-    int countPrimes(int n) {
-        if (n < 2) {
-            return 0;
-        }
-        
-        // Sieve of Eratosthenes's algorithm to find primes
-        // def: If we know that 2 is a prime, then 4, 6, 8, 10, 12 ... are not primes
-        // we can use a for loop to detect all multiples of 2 within the given range
-        
-        // We assume that all numbers within [0,n-1] are primes at first
-        // Then we'll strike out all multiples of a prime.
-        // int total = n; // 0 ~ n - 1
-        // total -= 2; // We know that 0 and 1 are NOT primes
-        bool is_prime[n];
-        for (int i = 0; i < n; i++) {
-            is_prime[i] = true;
-        }
-        is_prime[0] = false;
-        is_prime[1] = false;
-        int total = n - 2;
-        
-        for (int i = 2; i <= std::sqrt(n); i++) {
-            if (is_prime[i]) {
-                for (int j = 2; i * j < n; j++) {
-                    if (is_prime[i * j]) {
-                        is_prime[i * j] = false;
-                        total--;
-                    }
-                }
-            }
-        }
-        
-        return total;
+  int countPrimes(int n) {
+    if (n < 2) {
+      return 0;
     }
+
+    // less than a non-negative number, n
+    bool is_prime[n]; // 0 1 2 ... n - 1
+    for (int i = 0; i < n; i++) {
+      is_prime[i] = true;
+    }
+    is_prime[0] = false;
+    is_prime[1] = false;
+    int total = n - 2;
+
+    for (int i = 2; i <= std::sqrt(n); i++) {
+      if (is_prime[i]) {
+        for (int j = 2; i * j < n; j++) {
+          int multiple = i * j;
+          if (is_prime[multiple]) {
+            is_prime[multiple] = false;
+            total--;
+          }
+        }
+      }
+    }
+    return total;
+  }
 };
