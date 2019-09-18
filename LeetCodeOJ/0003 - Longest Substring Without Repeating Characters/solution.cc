00cba1c986e29c6c4dbd2d9e8e299d6d91889c0e
Marco Wang <m.aesophor@gmail.com>
2019-09-18 15:00:20 +0800

    wtf

---
+++ LeetCodeOJ/0003 - Longest Substring Without Repeating Characters/solution.cc
@@ -0,0 +1,34 @@
+// Runtime: 96 ms, faster than 15.07% of C++ online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 10.7 MB, less than 66.17% of C++ online submissions for Longest Substring Without Repeating Characters.
+
+class Solution {
+ public:
+  int lengthOfLongestSubstring(const string& s) {
+    std::unordered_map<char, int> m; // <char, last_appeared_idx>
+    int left = 0;
+    int result = 0;
+    
+    for (int i = 0; i < s.size(); i++) {
+      char c = s[i];
+      auto it = m.find(c);
+
+      if (it != m.end()) {
+        left = std::max(left, it->second + 1);
+      }
+      
+      result = std::max(result, i - left + 1);
+      std::cout << "left: " << left << " result: " << result << std::endl;
+      m[c] = i; // update last appeared position
+    }
+    
+    return result;
+  }
+};
+
+// dvdf
+// 0123
+
+// left = 1
+// result = 0
+// <d,2> <v,1>
+// 
