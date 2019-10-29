e6aedc63d63621b01a8ca29cec10d297b4fe6851
Marco Wang <m.aesophor@gmail.com>
2019-10-30 00:27:44 +0800

    std::this_thread::yield

--- LeetCodeOJ/1115 - Print FooBar Alternately/solution.cc
+++ LeetCodeOJ/1115 - Print FooBar Alternately/solution.cc
@@ -1,24 +1,24 @@
-// Runtime: 332 ms, faster than 73.81% of C++ online submissions for Print FooBar Alternately.
-// Memory Usage: 10.5 MB, less than 100.00% of C++ online submissions for Print FooBar Alternately.
+// Runtime: 280 ms, faster than 92.02% of C++ online submissions for Print FooBar Alternately.
+// Memory Usage: 10.6 MB, less than 100.00% of C++ online submissions for Print FooBar Alternately.
 
 class FooBar {
  public:
   FooBar(int n) : n_(n), foo_should_print_(true) {}
 
-  void foo(function<void()> printFoo) {
+  void foo(const function<void()>& printFoo) {
     for (int i = 0; i < n_; i++) {
       while (!foo_should_print_) {
-        this_thread::sleep_for (std::chrono::nanoseconds(1));
+        this_thread::yield();
       }
       printFoo(); // outputs "foo"
       foo_should_print_ = false;
     }
   }
 
-  void bar(function<void()> printBar) {
+  void bar(const function<void()>& printBar) {
     for (int i = 0; i < n_; i++) {
       while (foo_should_print_) {
-        this_thread::sleep_for (std::chrono::nanoseconds(1));
+        this_thread::yield();
       }
       printBar(); // outputs "bar"
       foo_should_print_ = true;
