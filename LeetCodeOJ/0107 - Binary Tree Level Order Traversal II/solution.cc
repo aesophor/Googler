10fe736c863cdd4290479f423578dc84555eac5e
Marco Wang <m.aesophor@gmail.com>
2019-11-01 22:11:22 +0800

    5555555

---
+++ LeetCodeOJ/0107 - Binary Tree Level Order Traversal II/solution.cc
@@ -0,0 +1,43 @@
+// Runtime: 4 ms, faster than 95.28% of C++ online submissions for Binary Tree Level Order Traversal II.
+// Memory Usage: 13.8 MB, less than 97.30% of C++ online submissions for Binary Tree Level Order Traversal II.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    if (!root) {
+      return {};
+    }
+    
+    std::queue<TreeNode*> q;
+    q.push(root);
+    
+    while (!q.empty()) {
+      size_t qSize = q.size();
+      result_.push_back({});
+      
+      for (size_t i = 0; i < qSize; i++) {
+        TreeNode* node = q.front();
+        q.pop();
+        result_.back().push_back(node->val);
+
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);   
+      }
+    }
+    
+    std::reverse(result_.begin(), result_.end());
+    return result_;
+  }
+
+ private:
+  vector<vector<int>> result_;
+};
