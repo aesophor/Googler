a54fc6ba9ed970fba51f86346ee4737ff14b5916
Marco Wang <m.aesophor@gmail.com>
2019-11-03 00:46:55 +0800

    okok

--- LeetCodeOJ/0200 - Number of Islands/solution.cc
+++ LeetCodeOJ/0200 - Number of Islands/solution.cc
@@ -1,5 +1,5 @@
-// Runtime: 28 ms, faster than 10.68% of C++ online submissions for Number of Islands.
-// Memory Usage: 12.2 MB, less than 12.36% of C++ online submissions for Number of Islands.
+// Runtime: 16 ms, faster than 59.11% of C++ online submissions for Number of Islands.
+// Memory Usage: 12.3 MB, less than 12.36% of C++ online submissions for Number of Islands.
 
 class Solution {
  public:
@@ -15,53 +15,35 @@ class Solution {
     // Grid BFS: search all islands
     int numIsland = 0;
     
-    while (hasIslandsToSearch(grid)) {
-      std::queue<std::pair<int, int>> q;
-      q.push(getNextLand(grid));
-      
-      while (!q.empty()) {
-        int row = q.front().first;
-        int col = q.front().second;
-        char& c = grid[row][col];
-        q.pop();
-
-        if (c == '0') {
-          continue;
-        }
-
-        c = '0'; // change this shit to water
-        if (row - 1 >= 0) q.push({row - 1, col});
-        if (col - 1 >= 0) q.push({row, col - 1});
-        if (row + 1 <= rowSize - 1) q.push({row + 1, col});
-        if (col + 1 <= colSize - 1) q.push({row, col + 1});
-      }   
-      
-      numIsland++;
-    }
-   
-    return numIsland;
-  }
-  
- private:
-  std::pair<int, int> getNextLand(vector<vector<char>>& grid) {
     for (int i = 0; i < grid.size(); i++) {
       for (int j = 0; j < grid[i].size(); j++) {
-        if (grid[i][j] == '1') {
-          return {i, j};
+        if (grid[i][j] == '0') {
+          continue;
         }
+        
+        numIsland++;
+        std::queue<std::pair<int, int>> q;
+        q.push({i, j});
+
+        while (!q.empty()) {
+          int row = q.front().first;
+          int col = q.front().second;
+          char& c = grid[row][col];
+          q.pop();
+
+          if (c == '0') {
+            continue;
+          }
+
+          c = '0'; // change this shit to water
+          if (row - 1 >= 0) q.push({row - 1, col});
+          if (col - 1 >= 0) q.push({row, col - 1});
+          if (row + 1 <= rowSize - 1) q.push({row + 1, col});
+          if (col + 1 <= colSize - 1) q.push({row, col + 1});
+        }     
       }
     }
-    return {-1, -1};
-  }
-  
-  bool hasIslandsToSearch(vector<vector<char>>& grid) {
-    for (const auto& row : grid) {
-      for (auto c : row) {
-        if (c == '1') {
-          return true;
-        }
-      }
-    }
-    return false;
+      
+    return numIsland;
   }
 };
