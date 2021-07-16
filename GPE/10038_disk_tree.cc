b08f07cc7272c8a952a861c8d2d1388afd9cd0f6
Marco Wang <m.aesophor@gmail.com>
2021-07-16 11:38:00 +0800

    GPE: 10038: use C++98

--- GPE/10038_disk_tree.cc
+++ GPE/10038_disk_tree.cc
@@ -8,9 +8,9 @@ class Node {
 
   Node* get_or_create_child(const string& name) {
     // If the child already exists, return it.
-    for (auto child : _children) {
-      if (child->_name == name) {
-        return child;
+    for (int i = 0; i < (int) _children.size(); i++) {
+      if (_children[i]->_name == name) {
+        return _children[i];
       }
     }
 
@@ -24,6 +24,11 @@ class Node {
   vector<Node*> _children;
 };
 
+struct cmp {
+  bool operator ()(Node* n1, Node* n2) {
+    return n1->_name < n2->_name;
+  }
+};
 
 void dfs(Node* node, const int depth) {
   if (!node) {
@@ -39,42 +44,40 @@ void dfs(Node* node, const int depth) {
 
   sort(node->_children.begin(),
        node->_children.end(),
-       [](Node* n1, Node* n2) { return n1->_name < n2->_name; });
+       cmp());
 
-  for (auto child : node->_children) {
-    dfs(child, depth + 1);
+  for (int i = 0; i < (int) node->_children.size(); i++) {
+    dfs(node->_children[i], depth + 1);
   }
 }
 
 
 int main() {
   int N;
+  cin >> N;
 
-  while (cin >> N) {
-    Node* root_node = new Node("");
+  Node* root_node = new Node("");
 
-    for (int i = 0; i < N; i++) {
-      string filename;
-      cin >> filename;
+  for (int i = 0; i < N; i++) {
+    string filename;
+    cin >> filename;
 
-      Node* current = root_node;
-      size_t begin = 0;
+    Node* current = root_node;
+    size_t begin = 0;
 
-      while (begin != filename.npos) {
-        size_t next = filename.find('\\', begin);
-        string component_name = filename.substr(begin, next - begin);
+    while (begin != filename.npos) {
+      size_t next = filename.find('\\', begin);
+      string component_name = filename.substr(begin, next - begin);
 
-        current = current->get_or_create_child(component_name);
+      current = current->get_or_create_child(component_name);
 
-        if (next == filename.npos) {
-          break;
-        }
-        begin = next + 1;
+      if (next == filename.npos) {
+        break;
       }
+      begin = next + 1;
     }
-
-    // Filesystem tree preorder DFS
-    dfs(root_node, -1);
-    cout << endl;
   }
+
+  // Filesystem tree preorder DFS
+  dfs(root_node, -1);
 }
